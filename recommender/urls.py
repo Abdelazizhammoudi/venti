7c0577955d877f7fa5bfac35732aac174800eacc
azizhmd74 <98393434+azizhmd74@users.noreply.github.com>
2024-06-15 04:10:51 +0100

    Update urls.py

--- recommender/urls.py
+++ recommender/urls.py
@@ -3,5 +3,5 @@ from recommender import views
 
 
 urlpatterns = [
-    path('item/articles/<int:article_id>/item-similarity', views.ItemSimilarityView.as_view(), name='item_similarity'),
+    path('item/articles/<int:article_id>/item-similarity/', views.ItemSimilarityView.as_view(), name='item_similarity'),
 ]
